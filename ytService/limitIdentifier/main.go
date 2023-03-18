cf34a205adbfae63ee4150349075e9709f48c5ac
varshneyabhushan <me@bhushan.fun>
2023-03-18 17:45:59 +0530

    identify the limit upto which videos have to fetched

---
+++ ytService/limitIdentifier/main.go
@@ -0,0 +1,35 @@
+package limitidentifier
+
+//tracks if videos limit reached or not
+type LimitIdentifier struct {
+	From string
+	Limit string
+}
+
+func (tracker *LimitIdentifier) AdvanceLimit() {
+	tracker.Limit = tracker.From
+	tracker.From = ""
+}
+
+type IdProvider interface {
+	GetId() string
+}
+
+func (tracker *LimitIdentifier) Scrutinise(idProviders []IdProvider) int {
+	totalValidProviders := 0
+
+	if len(idProviders) != 0 && len(tracker.From) == 0 {
+		tracker.From = idProviders[0].GetId()
+	}
+
+	for _, idProvider := range idProviders {
+		if idProvider.GetId() == tracker.Limit {
+			tracker.AdvanceLimit()
+			break
+		}
+
+		totalValidProviders += 1
+	}
+
+	return totalValidProviders
+}
