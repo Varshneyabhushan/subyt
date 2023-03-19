8c9ba91d9b940a56db02cfa6ee4f6e5c63d28345
varshneyabhushan <me@bhushan.fun>
2023-03-20 02:04:38 +0530

    implement delayTracker

---
+++ ytService/jobExecution/delayTracker.go
@@ -0,0 +1,36 @@
+package jobexecution
+
+import "time"
+
+type DelayTracker struct {
+	defaultDelay time.Duration
+	currentDelay time.Duration
+	delayCount   int
+}
+
+func NewDelayTracker(defaultDelay time.Duration) DelayTracker {
+	return DelayTracker{defaultDelay: defaultDelay, currentDelay: defaultDelay}
+}
+
+func (tracker DelayTracker) Delay() time.Duration {
+	return tracker.currentDelay
+}
+
+func (tracker DelayTracker) Reset() {
+	tracker.currentDelay = tracker.defaultDelay
+	tracker.delayCount = 0
+}
+
+// ProportionalDelay ProportionDelay increases delay proportional to totalDelays
+func (tracker DelayTracker) ProportionalDelay() time.Duration {
+	tracker.delayCount += 1
+	tracker.currentDelay += tracker.currentDelay
+	return tracker.Delay()
+}
+
+// ExponentialBackOff doubles delay by 2
+func (tracker DelayTracker) ExponentialBackOff() time.Duration {
+	tracker.delayCount += 1
+	tracker.currentDelay *= 2
+	return tracker.Delay()
+}
--- ytService/jobExecution/syncVideosJob.go
+++ ytService/jobExecution/syncVideosJob.go
@@ -1,7 +1,6 @@
 package jobexecution
 
 import (
-	"errors"
 	"log"
 	"time"
 	"ytservice/env"
@@ -17,13 +16,14 @@ func MakeSyncVideosJob(
 	config env.SchedulerConfig,
 ) RecurringJob {
 
-	waitingTime := time.Duration(config.RequestCoolDown) * time.Second
-	syncCoolDown := time.Duration(config.SyncCoolDown) * time.Second
+	delayTracker := NewDelayTracker(config.RequestCoolDown)
+	syncCoolDown := config.SyncCoolDown
 
 	return func() (time.Duration, error) {
 		response, err := fetcher.GetNext(checkPoint.NextPageToken)
 		if err != nil {
-			return 0, errors.New("error while getting videos : " + err.Error())
+			log.Println("error while getting videos : ", err)
+			return delayTracker.ExponentialBackOff(), nil
 		}
 
 		newCheckPoint, totalValidVideos := NewCheckpoint(*checkPoint, response)
@@ -31,19 +31,21 @@ func MakeSyncVideosJob(
 		//add videos to videosService
 		if err = service.AddVideos(response.Videos[:totalValidVideos]); err != nil {
 			log.Println("error while adding videos : " + err.Error())
-			return waitingTime, nil
+			return delayTracker.ProportionalDelay(), nil
 		}
+
+		delayTracker.Reset()
 
 		//when checkpoint is not saved, oldCheckpoint is reused
 		if err = newCheckPoint.Save(); err != nil {
 			log.Println("error while updating checkpoint", err)
-			return waitingTime, nil
+			return delayTracker.Delay(), nil
 		}
 		*checkPoint = newCheckPoint
 
 		//when pagination continues, sync shouldn't cool down
 		if len(response.NextPageToken) != 0 {
-			return waitingTime, nil
+			return delayTracker.Delay(), nil
 		}
 
 		return syncCoolDown, nil
