4239e0fef54bf86ffffee4acbca1b1dc5e17cc1f
varshneyabhushan <me@bhushan.fun>
2023-03-20 02:38:45 +0530

    return pointer

--- ytService/jobExecution/delayTracker.go
+++ ytService/jobExecution/delayTracker.go
@@ -8,28 +8,28 @@ type DelayTracker struct {
 	delayCount   int
 }
 
-func NewDelayTracker(defaultDelay time.Duration) DelayTracker {
-	return DelayTracker{defaultDelay: defaultDelay, currentDelay: defaultDelay}
+func NewDelayTracker(defaultDelay time.Duration) *DelayTracker {
+	return &DelayTracker{defaultDelay: defaultDelay, currentDelay: defaultDelay}
 }
 
-func (tracker DelayTracker) Delay() time.Duration {
+func (tracker *DelayTracker) Delay() time.Duration {
 	return tracker.currentDelay
 }
 
-func (tracker DelayTracker) Reset() {
+func (tracker *DelayTracker) Reset() {
 	tracker.currentDelay = tracker.defaultDelay
 	tracker.delayCount = 0
 }
 
 // ProportionalDelay ProportionDelay increases delay proportional to totalDelays
-func (tracker DelayTracker) ProportionalDelay() time.Duration {
+func (tracker *DelayTracker) ProportionalDelay() time.Duration {
 	tracker.delayCount += 1
 	tracker.currentDelay += tracker.currentDelay
 	return tracker.Delay()
 }
 
 // ExponentialBackOff doubles delay by 2
-func (tracker DelayTracker) ExponentialBackOff() time.Duration {
+func (tracker *DelayTracker) ExponentialBackOff() time.Duration {
 	tracker.delayCount += 1
 	tracker.currentDelay *= 2
 	return tracker.Delay()
