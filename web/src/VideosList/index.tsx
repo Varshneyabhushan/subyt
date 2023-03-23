d24dfa956fbdb790f9b5124a21587c3ac8d4dd7b
varshneyabhushan <me@bhushan.fun>
2023-03-24 00:24:07 +0530

    link to youtube video

--- web/src/VideosList/index.tsx
+++ web/src/VideosList/index.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from '@mui/material';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar'
@@ -9,13 +10,18 @@ interface CustomImageListProps {
   resource : VideoResource;
 }
 
+const youtubeVideo = (id : string) => `https://www.youtube.com/watch?v=${id}`
+
 export default function VideosList({ resource } : CustomImageListProps) {
   const videos = resource.read()
   
   return (
     <ImageList cols={4} gap={5} sx={{ paddingLeft : "15px", paddingRight : "15px" }}>
       {videos.map((item) => (
-        <ImageListItem key={item.Id} sx={{ width : 220 }}>
+        <ImageListItem 
+        key={item.Id} 
+        sx={{ width : 220, cursor : "pointer" }} 
+        onClick={() => window.open(youtubeVideo(item.Id), "_blank")}>
           <img
             src={`${item.Thumbnails[0].Url}`}
             // srcSet={`${item.thumbnail}?w=248&fit=crop&auto=format&dpr=2 2x`}
