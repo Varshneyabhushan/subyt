29d553af638d6ff20d7f03dd3dac6741d6de9f95
varshneyabhushan <me@bhushan.fun>
2023-03-23 16:59:33 +0530

    search button included

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Container, CssBaseline } from '@mui/material';
+import { Container, CssBaseline, Pagination, Stack, useStepContext } from '@mui/material';
 import Header from './Header';
 import VideosList from './VideosList';
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import VideosService, { VideosServiceConfig, VideoResource } from './services/videos';
 import ErrorBoundary from './Components/ErrorBoundary';
+import videosResource from './services/videosResource';
+import { FmdBadTwoTone } from '@mui/icons-material';
 
 const darkTheme = createTheme({
   palette: {
@@ -16,16 +18,47 @@ const darkTheme = createTheme({
 const config: VideosServiceConfig = { apiUrl: "http://localhost:3001" }
 const videosService = new VideosService(config)
 
-const initialVideos = videosService.getVideos(0, 20)
+const videosPerPage = 20
+const initialVideos = videosService.getVideos(0, videosPerPage)
 
 function App() {
 
   const [resource, setResource] = useState<VideoResource>(initialVideos)
+  const [videosCount, setVideosCount] = useState(100)
+  const [page, setPage] = useState(1)
+  const [searchTerm, setSearchTerm] = useState("")
+
+  //false for home, true for search
+  const [currentContext, setCurrentContext] = useState(false)
+
+  useEffect(() => {
+    let skip = (page - 1)* videosPerPage
+
+    //home page vs search page
+    let newResource = (currentContext) ? 
+      videosService.searchVideos(searchTerm, skip, videosPerPage) : 
+      videosService.getVideos(skip, videosPerPage);
+
+    setResource(newResource)
+  },
+  [page, setResource, searchTerm, currentContext])
+
+  function initSearch(term : string) {
+    setSearchTerm(term)
+    setPage(1)
+    setCurrentContext(true)
+  }
+
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Header />
+      <Header initSearch={initSearch}/>
+      <Pagination 
+        count={Math.ceil(videosCount/20)} 
+        page={page} 
+        onChange={(_, pageNumber) => setPage(pageNumber)} 
+        shape="rounded"/>
       <Container disableGutters sx={{
         overflow: "auto",
         maxHeight: "91vh",
